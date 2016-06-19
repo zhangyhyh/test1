99fe98e5fea6f2e6cf5192b8ff34d74e9f4c97fc
Louis Zhang <zhangyhyh@yeah.net>
2016-06-19 15:58:23 +0800

    add testjike3.8.py

--- test2.py
+++ test2.py
@@ -3,7 +3,16 @@
 # b = re.sub('xx(.*?)xx', 'xx%dxx'%222, a)
 # print(b)
 
-import re
-a = 'adfhxx123xxhfhxx456xxjfojf'
-b = re.findall('\d+', a)
-print(b)
+# import re
+# a = 'adfhxx123xxhfhxx456xxjfojf'
+# b = re.findall('\d+', a)
+# print(b)
+
+i = 0
+k=1000
+while k>1:
+    i+1
+    print k
+k=k/2
+
+print("i=", i)
---
+++ testjike3.8.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8 -*-
+import requests
+import re
+import sys
+reload(sys)
+# sys.setdefaultencoding("utf-8")
+class spider(object):
+
+if __name__ == '__main__':
+    classinfo = []
+    url = 'http://www.jikexueyuan.com/course/?pageNum=1'
+    jikespider = spider()
+    all_links = jikespider.changepage(url, 20)
+    
